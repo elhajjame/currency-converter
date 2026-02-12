abd972e9cf55520913942e752674c054ce40c049
elhajjame <el-hajjame@outlook.com>
2026-02-12 10:20:52 +0100

    exchange function

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-const key = 'fcecf16a847a48fddb7ee17d';
-const url = `https://v6.exchangerate-api.com/v6/${key}/latest/USD`
-
 // ---------------- selecting html element -------------------
 const swapBtn = document.querySelector('.swap');
 const convertBtn = document.querySelector('.convert-btn');
 
-const inputAmount = document.querySelector('.input-amount');
-const fromCurrency = document.querySelector('.from-currency');
-const toCurrency = document.querySelector('.to-currency');
-
+const inputAmountEl = document.querySelector('.input-amount');
+const fromCurrencyEl = document.querySelector('.from-currency');
+const toCurrencyEl = document.querySelector('.to-currency');
 const result = document.querySelector('#result');
+
+const key = '3229adba4653b81cc0f04222';
+
 function fetchData() {
+    const url = `https://v6.exchangerate-api.com/v6/${key}/latest/${fromCurrencyEl.value}`;
+
     fetch(url)
         .then(response => {
             if (!response.ok) {
@@ -19,24 +20,18 @@ function fetchData() {
             } return response.json();
         })
         .then(data => {
-            console.log(data);
-            const currencyCode = Object.keys(data.conversion_rates)
-            console.log(currencyCode);
-
-            fromCurrency.innerHTML = "";
-            toCurrency.innerHTML = "";
-
+            const currencyCode = Object.keys(data.conversion_rates);
             //---------------------- add currency to select options ---------------------
             currencyCode.forEach(code => {
                 const option1 = document.createElement("option");
                 option1.value = code;
                 option1.textContent = code;
-                fromCurrency.appendChild(option1);
+                fromCurrencyEl.appendChild(option1);
 
                 const option2 = document.createElement("option");
                 option2.value = code;
                 option2.textContent = code;
-                toCurrency.appendChild(option2);
+                toCurrencyEl.appendChild(option2);
             });
 
         })
@@ -45,12 +40,41 @@ function fetchData() {
 
 //-------------- swap the currency ---------------
 function swapCurrency() {
-    const temp = fromCurrency.value;
-    fromCurrency.value = toCurrency.value;
-    toCurrency.value = temp;
+    const temp = fromCurrencyEl.value;
+    fromCurrencyEl.value = toCurrencyEl.value;
+    toCurrencyEl.value = temp;
 }
-swapBtn.addEventListener('click', swapCurrency);
+//------------- convert function ----------------
+function exchange(){
+   const inputAmount = inputAmountEl.value;
+   const toCurrency = toCurrencyEl.value
+   const fromCurrency = fromCurrencyEl.value
+    const url = `https://v6.exchangerate-api.com/v6/${key}/latest/${fromCurrencyEl.value}`;
+    if (inputAmount === '') {    
+        return
+    }
+    fetch(url)
+    .then(response => {
+        if(!response.ok){
+            throw new console.error('could not get data');
+        }
+        return response.json();
+    })
+    .then(data=>{
+        let rate = data.conversion_rates[toCurrency];
+        let converted = rate * inputAmount;
+        
+        result.textContent =`${converted.toFixed(2)} ${toCurrency}`
+    })
+}
 
+swapBtn.addEventListener('click', swapCurrency);
+convertBtn.addEventListener('click', exchange);
+inputAmountEl.addEventListener('input', exchange);
+fromCurrencyEl.addEventListener("change", exchange)
+toCurrencyEl.addEventListener("change", exchange)
+
+exchange();
 fetchData();
 
 
